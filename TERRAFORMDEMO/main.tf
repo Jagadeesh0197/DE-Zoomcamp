a2f3b590321b12d0bb372eca429b7b6fc6150f9a
Jagadeesh0197 <111108127+Jagadeesh0197@users.noreply.github.com>
2026-01-27 15:42:34 +0000

    terraform to gcp buckets

--- TERRAFORMDEMO/main.tf
+++ TERRAFORMDEMO/main.tf
@@ -1,12 +1,29 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "7.16.0"
     }
   }
 }
 
 provider "google" {
-  # Configuration options
+  credentials="./keys/my-creds.json"
+  project = "corded-photon-476804-h4"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "corded-photon-476804-h4-terra-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
 }
