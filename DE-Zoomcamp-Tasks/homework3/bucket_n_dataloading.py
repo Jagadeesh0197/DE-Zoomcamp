2d81f0f57d88d608c7efc5277f11a039d046089b
Jagadeesh <komatireddyjagadeeshwarreddy@gmail.com>
2026-02-17 15:03:49 +0000

    hw3

---
+++ DE-Zoomcamp-Tasks/homework3/bucket_n_dataloading.py
@@ -0,0 +1,52 @@
+import os
+import sys
+from concurrent.futures import ThreadPoolExecutor
+
+BUCKET_NAME = "hw3_warehouse"
+
+BASE_URL = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-"
+MONTHS = [f"{i:02d}" for i in range(1,7)]
+DOWNLOAD_DIR = "."
+CHUNK_SIZE = 8 * 1024*  1024
+
+os.mkdirs(DOWNLOAD_DIR, exist_ok=True)
+
+bucket = client.bucket(BUCKET_NAME)
+
+def download_file(month):
+    url = f"{BASE_URL}{month}.parquet"
+
+def create_bucket(bucket_name):
+    try:
+        # Get bucket details
+        bucket = client.get_bucket(bucket_name)
+
+        # Check if the bucket belongs to the current project
+        project_bucket_ids = [bckt.id for bckt in client.list_buckets()]
+        if bucket_name in project_bucket_ids:
+            print(
+                f"Bucket '{bucket_name}' exists and belongs to your project. Proceeding..."
+            )
+        else:
+            print(
+                f"A bucket with the name '{bucket_name}' already exists, but it does not belong to your project."
+            )
+            sys.exit(1)
+
+    except NotFound:
+        # If the bucket doesn't exist, create it
+        bucket = client.create_bucket(bucket_name)
+        print(f"Created bucket '{bucket_name}'")
+    except Forbidden:
+        # If the request is forbidden, it means the bucket exists but you don't have access to see details
+        print(
+            f"A bucket with the name '{bucket_name}' exists, but it is not accessible. Bucket name is taken. Please try a different bucket name."
+        )
+        sys.exit(1)
+
+    
+if __name__ == "__main__":
+    create_bucket(BUCKET_NAME)
+
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        file_paths = list(executor.map(download_file, MONTHS))
